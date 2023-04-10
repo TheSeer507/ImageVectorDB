505fbd7b337daf9e2641ffc4e3a0856b0d55ac2f
Hasan Patel <mangafield@gmail.com>
2023-04-10 16:28:36 -0500

    Initial Files
    Base Files

---
+++ index.js
@@ -0,0 +1,37 @@
+import weaviate from 'weaviate-ts-client';
+import { readFileSync, writeFileSync } from 'fs';
+
+const client = weaviate.client({
+    scheme: 'http',
+    host: 'localhost:8080',
+});
+
+const schemaRes = await client.schema.getter().do();
+
+console.log(schemaRes)
+
+const img = readFileSync('./img/cat-inside.jpg');
+
+const b64 = Buffer.from(img).toString('base64');
+
+await client.data.creator()
+  .withClassName('Meme')
+  .withProperties({
+    image: b64,
+    text: 'cat meme'
+  })
+  .do();
+
+
+const test = Buffer.from( readFileSync('./test.jpg') ).toString('base64');
+
+const resImage = await client.graphql.get()
+  .withClassName('Meme')
+  .withFields(['image'])
+  .withNearImage({ image: test })
+  .withLimit(1)
+  .do();
+
+// Write result to filesystem
+const result = resImage.data.Get.Meme[0].image;
+writeFileSync('./result.jpg', result, 'base64');
